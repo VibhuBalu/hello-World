4d992f1eea882af18927942774969792a184ee1a
VibhuBalu <76510718+VibhuBalu@users.noreply.github.com>
2020-12-25 21:43:40 +0900

    my first programs

---
+++ my 1 program.py
@@ -0,0 +1,14 @@
+print('Hello guys!')
+person =input('My name is...?')
+age =input('My age is...?')
+place =input('I live in...?')
+grade =input('I am...?')
+school =input('I study in...?')
+Birthay =input('My birthday is on...?')
+animal =input('My favourite animal is...?')
+color =input('My favourite color is...?')
+hobby =input('My hobby is...?')
+sports =input('My favourite sport is...?')
+ 
+
+
