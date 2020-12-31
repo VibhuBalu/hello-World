1d6e2273db4576380343d8cb95e2e0d701d5ce3c
VibhuBalu <76510718+VibhuBalu@users.noreply.github.com>
2020-12-31 21:37:53 +0900

    Brain

---
+++ Brain program.py
@@ -0,0 +1,9 @@
+Organ = 'Brain -'
+Part1 = 'Cerebrum,'
+Part2 = 'Cerebellum,'
+Part3 = 'Brain stem.'
+Function1 = 'Memerising,'
+Function2 = 'Sendingrequests,'
+Function3 = 'Sensingoccuruntsaroundus.'
+message = Organ + Part1 + Part2 + Part3 + Function1 + Function2 + Function3 
+print(message)
